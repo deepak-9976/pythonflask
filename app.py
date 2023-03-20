48c744ec84a5a51f945d52af72a4c78698bab6b5
Replit user <>
2023-03-20 14:24:34 +0000

    template

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask import render_template, jsonify
+
 app = Flask(__name__)
 
-app.route('/')
+
+@app.route('/')
 def helloworld():
-  return "<p>Hello, World!</p>"
+  return render_template('home.html')
+
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", debug=True)
